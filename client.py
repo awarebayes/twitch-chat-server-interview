95033911ebfee39f536e5d985a888f80c9929c0c
Mikhail Scherbina <awarebayes@gmail.com>
2023-05-17 19:17:46 +0300

    push

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
+# this is a test client
+# not a server
 import socketio
 
 sio = socketio.Client()
 @sio.event
 def message(data):
     print('I received a message!', data)
-
 sio.connect('http://localhost:3000')
 sio.emit("message_sent", "a test message be here")
 sio.wait()
